b1217a75618b021da04a37449ea93e0e3acb21a0
zhangfx <zhangfx@minstone.com.cn>
2019-01-18 16:42:34 +0800

    添加service work

---
+++ dist/precache-manifest.3985f0282363dee999087441606e595a.js
@@ -0,0 +1,13 @@
+self.__precacheManifest = [
+  {
+    "url": "/main.44b8066963e473df81af.js"
+  },
+  {
+    "revision": "9141dd7d7c9d03b415390e5726b878c0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5884ded261bbe32e18fe",
+    "url": "/css/styles.css"
+  }
+];
